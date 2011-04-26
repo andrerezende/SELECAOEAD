a4763035e960145772faf40def7b7f35ba3beeb7
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2011-04-26 09:42:47 -0300

    alterado o valor do boleto

--- administracao/boleto/boleto_bb.php
+++ administracao/boleto/boleto_bb.php
@@ -65,7 +65,7 @@ if (empty($objinscrito)) {
 $dias_de_prazo_para_pagamento = 1;
 $taxa_boleto = 0.0;
 $data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006";
-$valor_cobrado = "20,00"; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+$valor_cobrado = "10,00"; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
 $valor_cobrado = str_replace(",", ".",$valor_cobrado);
 $valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 

3bcc43dc32299f305c5c834dea76c1f40a513435
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2011-05-17 10:29:46 -0300

    pasta desnecessaria removida

--- administracao/banco de dados/query_aluno_curso.sql
+++
@@ -1,6 +0,0 @@
-SELECT i.id, i.numinscricao AS Inscricao, i.nome AS Nome, i.campus AS Campus, cp.nome AS Campus, c.cod_curso AS CodCurso, c.nome AS Curso, l.id AS CodLocal, l.nome AS LocalProva
-FROM inscrito i, curso c, inscrito_curso ic, localprova l, campus cp
-WHERE c.cod_curso = ic.cod_curso
-AND i.id = ic.id_inscrito
-AND i.localprova = l.id
-AND i.campus = cp.id

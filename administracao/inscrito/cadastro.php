28c42bf2d699cfdff533d81aab87b523e238883d
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2011-05-19 09:29:46 -0300

    alteração no título

--- administracao/inscrito/cadastro.php
+++ administracao/inscrito/cadastro.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-	<title>Processo Seletivo para Ingresso de Estudantes - 2011.2</title>
+	<title>Processo Seletivo para Cursos Técnicos à Distância - 2011.2</title>
 	<link href="../../estilo_selecao.css" rel="stylesheet" type="text/css" />
 	<script type="text/javascript" src="../../js/jquery-1.3.2.min.js"></script>
 	<script type="text/javascript" src="../../js/jquery.maskedinput-1.3.min.js"></script>
